13b023016ba08148e11e2437b6c549a14f8685c5
Emre Aydin <emre.aydn.1996@gmail.com>
2018-07-30 16:33:21 +0300

    fix about page
    fixed store buttons

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -15,17 +15,15 @@ class AboutUs extends React.Component{
                                 <p>BUGECE Manager, gece kulübü kütürünü, mekan işletmecilerini ve organizatörlerini anlayan, hedef kitle odaklı yeni nesil bilet platformudur.</p>
                                 <p>Uygulamamız, gelişmiş web paneli ve kullanımı kolay özelliklerle, etkinlikleriniz en verimli şekilde yönetirken hizmet kalitenizi yükseltir.</p>
                                 <p style={{fontWeight:'bold'}}>En köklü gece kulübü ve markaları tarafından kullanılan, sektör lideri biletleme platformunda, Türkiye'nin en geniş çevrimiçi elektronik müzik topluluğuna hitap ederek, etkinliklerinizin görünürlüğünü arttırın!</p>
-                                <div id="store">
-                                    <div className="row">
-                                        <div className="col-md-1"></div>
-                                        <div className="col-md-5">
-                                            <a href="https://play.google.com/store/apps/details?id=com.app.bugece&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"><img alt="Google Play'den indirin" className="store-img" src="https://play.google.com/intl/en_us/badges/images/generic/tr_badge_web_generic.png"/></a>
-                                        </div>
-                                        <div className="col-md-5">
-                                            <a href="https://itunes.apple.com/tr/app/bugece/id1161536177?l=tr&mt=8"><img alt="App Store'dan indirin" className="store-img" src={require('../img/appStore.svg')}/></a>
-                                        </div>
-                                        <div className="col-md-1"></div>
+                                <div className="row" id="store">
+                                    <div className="col-md-1"></div>
+                                    <div className="col-md-5">
+                                        <a href="https://play.google.com/store/apps/details?id=com.app.bugece&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"><img alt="Google Play'den indirin" className="store-img" src="https://play.google.com/intl/en_us/badges/images/generic/tr_badge_web_generic.png"/></a>
                                     </div>
+                                    <div className="col-md-5">
+                                        <a href="https://itunes.apple.com/tr/app/bugece/id1161536177?l=tr&mt=8"><img alt="App Store'dan indirin" className="store-img" src={require('../img/appStore.svg')}/></a>
+                                    </div>
+                                    <div className="col-md-1"></div>
                                 </div>
                             </div>
                         </div>
